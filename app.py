9d0d01b692a0fe948e2ff29c8336deabc85a6cbd
AfreenAman <67589945+AfreenAman@users.noreply.github.com>
2020-11-05 11:59:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ from gensim.summarization import summarize
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+import nltk
+nltk.download('punkt')
+
 
 # Sumy Summarization
 def sumy_summarizer(docx):
